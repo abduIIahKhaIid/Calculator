df60ba4304dbfb2874eb503f92c3cf1fb43399f9
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-10-08 22:19:52 +0500

    first commit

--- Calculator.ts
+++ Calculator.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
@@ -32,20 +33,21 @@ async function main() {
     let answer: number = await Question(i);
     operator = await Operator();
     do {
-        
+
         if (operator == '+Addition') {
             i++;
             let answer2 = await Question(i);
             sum = sum + answer + answer2;
+            answer = 0;
             console.log(sum);
-            
+
         }
         else if (operator == '-Substraction') {
             i++;
             let answer2 = await Question(i);
             sum = (answer - answer2);
             console.log(sum);
-            
+
         }
         operator = await Operator();
     } while (operator != '=Equal');
